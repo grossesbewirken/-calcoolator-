2b59aed05f4d110e980d04a2a0fe000d7ced4b01
magross <grossesbewirken@gmail.com>
2022-10-05 08:57:27 +0200

    sfsg

---
+++ calcoolator/src/DigitButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from "./App.js"
+
+export default function DigitButton ({dispatch, digit}){
+    return <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit }})}>{digit}</button>
+}
---
+++ calcoolator/src/OperationButton.js
@@ -0,0 +1,7 @@
+import { ACTIONS } from "./App.js"
+
+export default function OperationButton ({dispatch, operation}){
+    return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}>
+        {operation}
+        </button>
+}
